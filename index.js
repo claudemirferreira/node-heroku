51e307d1c78c41916ba189f2261c500d2308cabd
claudemir ramos ferreira <claudemirramosferreira@gmail.com>
2020-01-09 08:48:39 -0400

    add 1 service

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require("cors");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended: true}))
+  .get('/', function (req, res) {
+    return res.send({ error: true, message: "hello" });
+  })
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
